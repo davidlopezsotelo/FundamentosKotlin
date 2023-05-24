6f3ad9631c3d3b32dd4787d0abeac0f71b7288b7
davidlopezsotelo <davidaquarium@yahoo.es>
2023-05-24 15:30:54 +0200

    Initial commit

---
+++ app/src/main/java/com/davidlopez/fundamentoskotlin/Principal.kt
@@ -0,0 +1,18 @@
+package com.davidlopez.fundamentoskotlin
+
+fun main (){
+
+    //llamamos a la funcion y la pasamos el string:
+
+    newTopic("Hola, kotlin")
+
+
+}
+
+// funcion que recibe un string por parametro llamado topic
+
+fun newTopic(topic:String){
+
+    //imprime en consola lo que le pasemos al parametro mediante la funcion topic
+    println(topic)
+}
